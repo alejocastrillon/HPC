282d4c18bb17270a772d84093e47547e12ded988
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2020-08-27 21:23:36 -0500

    Ajuste: Multiplicacion de matrices en paralelo con hilos

--- malmutParallel.cc
+++ malmutParallel.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <thread>
+#include <thread>
+#include <functional>
 #include "timer.hh"
 
 using namespace std;
@@ -8,11 +11,13 @@ using namespace std;
 /**
  * Initialize an matrix.
  *
+ * @param columns Number of columns.
  * @param dimensions Number of dimensions.
  * @return Matrix initialized
  */
-vector<vector<int> > initializeMatrix(int dimensions) {
-    vector<vector<int> > a;
+vector<vector<int>> initializeMatrix(int dimensions)
+{
+    vector<vector<int>> a;
     for (size_t i = 0; i < dimensions; i++)
     {
         vector<int> ac;
@@ -26,26 +31,46 @@ vector<vector<int> > initializeMatrix(int dimensions) {
 }
 
 /**
- * Compute the matrix multiplication.
+ * Compute the  result of a cell of matrix multiplication.
+ *
+ * @param a Matrix A
+ * @param b Matrix B
+ * @param i Column index
+ * @param j Row index
+ * @param sum Result of a multiplication cell
+ */
+void computeCell(const vector<vector<int>> &a, const vector<vector<int>> &b, int i, int j, int &sum)
+{
+    for (size_t k = 0; k < a.size(); k++)
+    {
+        sum += a[i][k] * b[k][j];
+    }
+}
+
+/**
+ * Assignment the matrix multiplication at threads.
  *
  * @param a Matrix A.
  * @param b Matrix B.
  * @return Matrix multiplication between A and B
  */
-vector<vector<int> > computeMult(vector<vector<int> > a, vector<vector<int> > b) {
-    vector<vector<int> > mult;
+vector<vector<int>> assingMult(vector<vector<int>> a, vector<vector<int>> b)
+{
+    vector<vector<int>> mult;
+    vector<thread> threads;
     for (size_t i = 0; i < a.size(); i++)
     {
-        vector<int> res(a.size(), 0);
+        mult.push_back(vector<int>(a.size(), 0));
         for (size_t j = 0; j < a[i].size(); j++)
         {
-            for (size_t k = 0; k < a[i].size(); k++)
-            {
-                res[j] += a[i][k] * b[k][j];
-            }
+            int &result = mult[i][j];
+            threads.push_back(thread(computeCell, cref(a), cref(b), i, j, ref(result)));
         }
-        mult.push_back(res);
     }
+    /* const auto processorCount = thread::hardware_concurrency();
+    int iteration = threads.size() / processorCount; */
+    for (thread &t : threads)
+        t.join();
     return mult;
 }
 
@@ -54,7 +79,8 @@ vector<vector<int> > computeMult(vector<vector<int> > a, vector<vector<int> > b)
  *
  * @param a Matrix instance.
  */
-void visualizeArray(vector<vector<int> > a) {
+void visualizeArray(vector<vector<int>> a)
+{
     for (size_t i = 0; i < a.size(); i++)
     {
         for (size_t j = 0; j < a[i].size(); j++)
@@ -62,12 +88,12 @@ void visualizeArray(vector<vector<int> > a) {
             cout << a[i][j] << "|";
             if (j == a[i].size() - 1)
             {
-                cout << "\n";
+                cout << endl;
                 for (size_t ch = 0; ch < (a.size() * 2) + a.size(); ch++)
                 {
                     cout << "-";
                 }
-                cout << "\n";
+                cout << endl;
             }
         }
     }
@@ -80,7 +106,7 @@ void visualizeArray(vector<vector<int> > a) {
  */
 int main()
 {
-    vector<vector<int> > a, b, mult;
+    vector<vector<int>> a, b, mult;
     int dimensions;
     cout << "Ingrese el número de dimensiones: ";
     cin >> dimensions;
@@ -88,18 +114,17 @@ int main()
     a = initializeMatrix(dimensions);
     b = initializeMatrix(dimensions);
 
-    mult = computeMult(a, b);
-    cout << "Elementos de la matriz A: \n";
+    mult = assingMult(a, b);
+    cout << "Elementos de la matriz A: " << endl;
     visualizeArray(a);
 
-    cout << "Elementos de la matriz B: \n";
+    cout << "Elementos de la matriz B: " << endl;
     visualizeArray(b);
 
-    cout << "Resultado:  \n";
+    cout << "Resultado: " << endl;
     visualizeArray(mult);
 
     cout << "Tiempo de ejecución: " << t.elapsed() << "ms" << endl;
 
     return 0;
-
 }
