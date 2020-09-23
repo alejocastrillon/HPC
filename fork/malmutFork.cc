d5a1146cafa4082ab855c497eb226aaf577d45d2
Alejandro Castrillon <alejandro.castrillon@utp.edu.co>
2020-09-23 10:54:57 -0500

    Implementacion de Fork

--- fork/malmutFork.cc
+++ fork/malmutFork.cc
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <stdlib.h>
-#include <unistd.h>
 #include <vector>
+#include <thread>
+#include <unistd.h>
+#include <functional>
+#include <sys/wait.h>
 #include "../timer.hh"
 
 using namespace std;
 
 /**
- * Initialize an matrix.
+ * Initialize a matrix.
  *
  * @param dimensions Number of dimensions.
  * @return Matrix initialized
@@ -27,36 +30,10 @@ int **initializeMatrix(int dimensions)
 }
 
 /**
- * Compute the matrix multiplication.
- *
- * @param a Matrix A.
- * @param b Matrix B.
- * @return Matrix multiplication between A and B
- */
-int **computeMult(int **a, int **b, int dimensions)
-{
-    int **mult = new int *[dimensions];
-    for (size_t i = 0; i < dimensions; i++)
-    {
-        mult[i] = new int[dimensions];
-        for (size_t j = 0; j < dimensions; j++)
-        {
-            mult[i][j] = 0;
-            for (size_t k = 0; k < dimensions; k++)
-            {
-                mult[i][j] += a[i][k] * b[k][j];
-            }
-            cout << mult[i][j] << " | ";
-        }
-        cout << endl;
-    }
-    return mult;
-}
-
-/**
  * Visualize a determinate matrix.
  *
- * @param a Matrix instance.
+ * @param a Matrix instance
+ * @param dimensions Matrix dimensions number
  */
 void visualizeArray(int **a, int dimensions)
 {
@@ -67,15 +44,65 @@ void visualizeArray(int **a, int dimensions)
             cout << a[i][j] << "|";
             if (j == dimensions - 1)
             {
-                cout << "\n";
-                for (size_t ch = 0; ch < (dimensions * 2) + dimensions; ch++)
-                {
-                    cout << "-";
-                }
-                cout << "\n";
+                cout << endl;
             }
         }
     }
+}
+
+/**
+ * Compute the  result of a cell of matrix multiplication.
+ *
+ * @param a Matrix A
+ * @param b Matrix B
+ * @param i Column index
+ * @param dimensions Matrix dimensions number
+ * @param sum Result of a multiplication cell
+ */
+void computeCell(int **&a, int **&b, int i, int dimensions, int *&sum)
+{
+    for (size_t j = 0; j < dimensions; j++)
+    {
+        sum[j] = 0;
+        for (size_t k = 0; k < dimensions; k++)
+        {
+            sum[j] += a[i][k] * b[k][j];
+        }
+    }
+}
+
+/**
+ * Assignment the matrix multiplication at threads.
+ *
+ * @param a Matrix A.
+ * @param b Matrix B.
+ * @param dimensions Matrix dimension number
+ * @return Matrix multiplication between A and B
+ */
+int **assingMult(int **a, int **b, int dimensions)
+{
+    int **mult = new int *[dimensions];
+    pid_t procesosHijos[dimensions];
+    int status[dimensions];
+    for (size_t i = 0; i < dimensions; i++)
+    {
+        mult[i] = new int[dimensions];
+        int *&result = mult[i];
+        if (procesosHijos[i] = fork() < 0)
+        {
+            cout << "No se pudo crear el subproceso" << endl;
+            exit(0);
+        }
+        else if (procesosHijos[i] == 0)
+        {
+            computeCell(ref(a), ref(b), i, dimensions, ref(result));
+        }
+    }
+    for (size_t i = 0; i < dimensions; i++)
+    {
+        waitpid(procesosHijos[i], &status[i], 0);
+    }
+    return mult;
 }
 
 /**
@@ -88,37 +115,21 @@ int main()
     int **a;
     int **b;
     int **mult;
-    int pid;
-    int pipefd[2];
     int dimensions;
     cout << "Ingrese el número de dimensiones: ";
     cin >> dimensions;
-    Timer t;
     a = initializeMatrix(dimensions);
     b = initializeMatrix(dimensions);
-
-    cout << "Elementos de la matriz A: \n";
+    Timer t;
+    cout << "Entro" << endl;
+    mult = assingMult(a, b, dimensions);
+    cout << "Elementos de la matriz A: " << endl;
     visualizeArray(a, dimensions);
 
-    cout << "Elementos de la matriz B: \n";
+    cout << "Elementos de la matriz B: " << endl;
     visualizeArray(b, dimensions);
-    pipe(pipefd);
-    if (pipefd < 0)
-    {
-        cout << "Pipe error" << endl;
-    }
-    pid = fork();
-    if (pid == -1)
-    {
-        cout << "Can't fork" << endl;
-    }
-    if (pid == 0)
-    {
-        mult = computeMult(a, b, dimensions);
-    }
-
-    /* cout << "Resultado:  \n";
-    visualizeArray(mult, dimensions); */
+    cout << "Resultado: " << endl;
+    visualizeArray(mult, dimensions);
 
     cout << "Tiempo de ejecución: " << t.elapsed() << "ms" << endl;
 
---
+++ fork/malmutForkTwo.cc
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "../timer.hh"
+
+using namespace std;
+
+/**
+ * Initialize an matrix.
+ *
+ * @param dimensions Number of dimensions.
+ * @return Matrix initialized
+ */
+int **initializeMatrix(int dimensions)
+{
+    int **a = new int *[dimensions];
+    for (size_t i = 0; i < dimensions; i++)
+    {
+        a[i] = new int[dimensions];
+        for (size_t j = 0; j < dimensions; j++)
+        {
+            a[i][j] = rand() % 100;
+        }
+    }
+    return a;
+}
+
+/**
+ * Compute the matrix multiplication.
+ *
+ * @param a Matrix A.
+ * @param b Matrix B.
+ * @return Matrix multiplication between A and B
+ */
+int **computeMult(int **a, int **b, int dimensions)
+{
+    int **mult = new int *[dimensions];
+    int status[dimensions];
+    pid_t procesosHijos[dimensions];
+    for (size_t i = 0; i < dimensions; i++)
+    {
+        mult[i] = new int[dimensions];
+        if ((procesosHijos[i] = fork()) < 0)
+        {
+            cout << "No es posible crear un subproceso" << endl;
+        }
+        else if (procesosHijos[i] == 0)
+        {
+            for (size_t j = 0; j < dimensions; j++)
+            {
+                for (size_t k = 0; k < dimensions; k++)
+                {
+                    mult[i][j] += a[i][k] * b[k][j];
+                }
+            }
+
+            for (size_t i = 0; i < dimensions; i++)
+            {
+                waitpid(procesosHijos[i], &status[i], 0);
+            }
+            
+        }
+    }
+    return mult;
+}
+
+/**
+ * Visualize a determinate matrix.
+ *
+ * @param a Matrix instance.
+ */
+void visualizeArray(int **a, int dimensions)
+{
+    for (size_t i = 0; i < dimensions; i++)
+    {
+        for (size_t j = 0; j < dimensions; j++)
+        {
+            cout << a[i][j] << "|";
+            if (j == dimensions - 1)
+            {
+                cout << "\n";
+                for (size_t ch = 0; ch < (dimensions * 2) + dimensions; ch++)
+                {
+                    cout << "-";
+                }
+                cout << "\n";
+            }
+        }
+    }
+}
+
+/**
+ * Main function.
+ * 
+ * @return State of the process
+ */
+int main()
+{
+    int **a;
+    int **b;
+    int **mult;
+    int dimensions;
+    cout << "Ingrese el número de dimensiones: ";
+    cin >> dimensions;
+    Timer t;
+    a = initializeMatrix(dimensions);
+    b = initializeMatrix(dimensions);
+
+    mult = computeMult(a, b, dimensions);
+    cout << "Elementos de la matriz A: \n";
+    visualizeArray(a, dimensions);
+
+    cout << "Elementos de la matriz B: \n";
+    visualizeArray(b, dimensions);
+
+    cout << "Resultado:  \n";
+    visualizeArray(mult, dimensions);
+
+    cout << "Tiempo de ejecución: " << t.elapsed() << "ms" << endl;
+
+    return 0;
+}
